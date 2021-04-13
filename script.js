15cfb67d03da404770a186e7d2bcc10929662f0b
SherieBach <Sherie.klappenbach@gmail.com>
2021-04-13 10:23:35 +0200

    added script file

---
+++ script.js
@@ -0,0 +1,16 @@
+var myScript = function(){
+
+function toggle(){
+    var allCards = document.getElementsByClassName('cards');
+    allCards.getElementsByClassName('.card-content').toggle();
+}
+
+
+
+
+
+
+
+};
+
+myScript();
