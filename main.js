4af4e8fd188a2faa795254df575663ac6162b749
SherieBach <Sherie.klappenbach@gmail.com>
2021-05-07 10:56:46 +0200

    added cards positioning and mobile viewport

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-const myScript = function(){
+const myScript = function () {
 
-const cards = document.querySelectorAll('.card-inner');
-    cards.forEach((cards) => {
-        cards.addEventListener('click', () => {
-            cards.classList.toggle('is-flipped')
+    const cards = document.querySelectorAll('.card-inner');
+    let cardArray = Array.from(cards);
+    console.log(cardArray, 'hej');
+    cardArray.forEach((card) => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('is-flipped')
+            console.log('after toggle');
+            // let otherCards = cardArray.filter( activeCard => activeCard.id !== card.id );
+            // console.log(otherCards);
+            // otherCards.forEach((notClicked) => {
+            //     notClicked.classList.remove('is-flipped');
+           // });
+        });
+
     });
 
+/*    let mouseCursor = document.querySelector('#cursor-letter-ark');
 
+    window.addEventListener('mousemove', cursor);
 
-});
-
-
-
-
-
-
+    function cursor(e) {
+        mouseCursor.style.top = e.pageY + 'px';
+        mouseCursor.style.left = e.pageX + 'px';
+    }*/
 };
 
 myScript();
